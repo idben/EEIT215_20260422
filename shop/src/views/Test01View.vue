56025a62f709c45a9874dad34033dce7433a124f
Yubin <yubin@ispan.com.tw>
2026-04-22 09:54:02 +0800

    test01 done

--- shop/src/views/Test01View.vue
+++ shop/src/views/Test01View.vue
@@ -3,13 +3,19 @@ import { ref, onMounted, onUnmounted } from 'vue';
 
 const windowWidth = ref(window.innerWidth);
 
+const handleResize = () => {
+    windowWidth.value = window.innerWidth;
+    console.log(window.innerWidth);
+}
+
 onMounted(() => {
     console.log("元件已掛載");
-    window.addEventListener('resize', () => {
-        windowWidth.value = window.innerWidth;
-        console.log(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+})
 
-    });
+onUnmounted(() => {
+    console.log("元件已卸載");
+    window.removeEventListener('resize', handleResize);
 })
 </script>
 <template>
