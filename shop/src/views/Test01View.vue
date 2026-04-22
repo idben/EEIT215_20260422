61bbbd85936fa88572c2c0cfa34ddfbf57b64db6
Yubin <yubin@ispan.com.tw>
2026-04-22 09:48:11 +0800

    onMounted window.innerWidth

--- shop/src/views/Test01View.vue
+++ shop/src/views/Test01View.vue
@@ -2,11 +2,20 @@
 import { ref, onMounted, onUnmounted } from 'vue';
 
 const windowWidth = ref(window.innerWidth);
+
+onMounted(() => {
+    console.log("元件已掛載");
+    window.addEventListener('resize', () => {
+        windowWidth.value = window.innerWidth;
+        console.log(window.innerWidth);
+
+    });
+})
 </script>
 <template>
     <h1>onMounted/onUnmounted 測試</h1>
     <p>
-        視窗寬度: {{ windowWidth }}px
+        視窗寬度: <span class="badge bg-danger text-white">{{ windowWidth }}</span>px
     </p>
 </template>
 <style scoped></style>
